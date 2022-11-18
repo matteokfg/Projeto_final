dbc531d7c08d2decd8db876ce088d5d4fe0b094b
Matteo K. F. Giuliani <67129306+matteokfg@users.noreply.github.com>
2022-11-18 11:10:58 -0300

    Update crud.py

--- projeto/crud.py
+++ projeto/crud.py
@@ -3,14 +3,18 @@
 """
 create database PetShop;          -- Cria o banco de dados
 use PetShop;                      -- Seleciona o banco para os próximos comandos
-
 /* As linhas acima não devem ser executas em serviços online como o sqlite oline*/
 
+create table Raca (
+	id		integer		primary key auto_increment,
+    nome	varchar(60)	unique not null
+);
 
 create table Especies (
 	id				integer 			primary key auto_increment,
 	nome			varchar(50)			unique  not null,
-	alimentacao		varchar(20)
+	alimentacao		varchar(20),
+    raca_id			integer				references Raca(id)
 );
 
 create table Animais (
@@ -18,9 +22,40 @@ create table Animais (
 	nome			varchar(50) 		not null,
 	data_nasc		date 				not null,
 	peso			decimal(10,2)		check (peso > 0),
-	cor				varchar(50),
+	pelagem			varchar(50)			not null,
+    sexo			char(1)				not null,
+    primeira_ida	date				check (primeira_ida < curdate()), /* ver se eh ativo */
+    ultima_ida		date				check (ultima_ida <= curdate()),
+    castrado		boolean				not null,
+	especie_id		integer				references Especies(id)
+); /* curdate() */
 
-	especie_id		int					references Especies(id)
+create table Cliente (
+	cpf			char(11)		primary key,
+    nome		varchar(60) 	not null,
+    logradouro	varchar(100)	not null,
+    numero		varchar(8)		not null,
+    bairro		varchar(30),
+    cidade		varchar(50)		not null,
+    estado		char(2)			not null, /* colocar em letra maiuscula (.upper)) */
+	animal_id	integer			references Animais(id)
+);
+
+create table Telefone (
+	cliente_id	integer,
+    ddd			char(2),
+    telefone	char(9),
+    
+    primary key(cliente_id, ddd, telefone),
+    foreign key(cliente_id) references Cliente(cpf)
+);
+
+create table Email (
+	cliente_id	integer,
+    email		varchar(100),
+    
+    primary key(cliente_id, email),
+    foreign key(cliente_id) references Cliente(cpf)
 );
 """
 #Fim
@@ -42,7 +77,7 @@ def conectarBD(host, usuario, senha, DB):
 
 #INSERT
 def insert_BD(nome, alimentacao=None, data_nasc=None, peso=None, cor=None, especie_id=None):
-    connection = conectarBD("localhost", "root", , "Login") #Recebe a conexão estabelecida com o banco
+    connection = conectarBD("localhost", "root", "admin", "Login") #Recebe a conexão estabelecida com o banco
     cursor = connection.cursor() #Cursor para comunicação com o banco, o cursor sabe o que o mysql precisa e o que o mysql retorna, fazendo o meio de campo entre o python e o mysql
 
     if alimentacao != None:
@@ -73,7 +108,7 @@ def insert_BD(nome, alimentacao=None, data_nasc=None, peso=None, cor=None, espec
 
 ###READ
 def read_BD(tabela):
-    connection = conectarBD("localhost", "root", , "Login") #Recebe a conexão estabelecida com o banco
+    connection = conectarBD("localhost", "root", "admin", "Login") #Recebe a conexão estabelecida com o banco
     cursor = connection.cursor() #Cursor para comunicação com o banco
 
     sql = "SELECT * FROM %s" #Realizando um select para mostrar todas as linhas e colunas da tabela
@@ -90,12 +125,12 @@ def read_BD(tabela):
         print(result) #imprime os registros existentes
 
 #UPDATE
-
-def update_BD(id, nome, alimentacao=None, data_nasc=None, peso=None, cor=None, especie_id=None, exclusividade=None):
-    connection = conectarBD("localhost", "root", , "Login") #Recebe a conexão estabelecida com o banco
+# SEPARAR PARA O UPDATE SER DIVIDIDO POR TABELA, ASSIM CADA TABELA VAI TER UM DEF UPDATE DIFERENTE, ESPECIFICO PARA CADA UM
+def update_BD(tabela, id=None, cpf=None, nome=None, alimentacao=None, data_nasc=None, peso=None, pelagem=None, sexo=None, primeira_ida=None, ultima_ida=None, castrado=None, especie_id=None, raca_id=None, logradouro=None, numero=None, bairro=None, cidade=None, estado=None, animal_id=None, exclusividade=None):
+    connection = conectarBD("localhost", "root", "admin", "Login") #Recebe a conexão estabelecida com o banco
     cursor = connection.cursor() #Cursor para comunicação com o banco
 
-    if alimentacao != None:
+    if tabela == 'Especies':
         if exclusividade == 'nome':
             sql = "UPDATE Especies SET nome = %s WHERE id = %s"
             data = (
@@ -108,14 +143,22 @@ def update_BD(id, nome, alimentacao=None, data_nasc=None, peso=None, cor=None, e
                 alimentacao,
                 id
             )
+        elif exclusividade == 'raca_id':
+            sql = "UPDATE Especies SET raca_id = %s WHERE id = %s"
+            data = (
+                raca_id,
+                id
+            )
         else:
-            sql = "UPDATE Especies SET nome = %s, alimentacao = %s WHERE id = %s"
+            sql = "UPDATE Especies SET nome = %s, alimentacao = %s, raca_id = %s WHERE id = %s"
             data = (
                 nome, 
                 alimentacao,
+                raca_id,
                 id
             )
-    else:
+
+    elif tabela == 'Animais':
         if exclusividade == 'nome':
             sql = "UPDATE Animais SET nome = %s WHERE id = %s"
             data = (
@@ -134,10 +177,34 @@ def update_BD(id, nome, alimentacao=None, data_nasc=None, peso=None, cor=None, e
                 peso,
                 id
             )
-        elif exclusividade == 'cor':
-            sql = "UPDATE Animais SET cor = %s WHERE id = %s"
+        elif exclusividade == 'pelagem':
+            sql = "UPDATE Animais SET pelagem = %s WHERE id = %s"
             data = (
-                cor,
+                pelagem,
+                id
+            )
+        elif exclusividade == 'sexo':
+            sql = "UPDATE Animais SET sexo = %s WHERE id = %s"
+            data = (
+                sexo,
+                id
+            )
+        elif exclusividade == 'primeira_ida':
+            sql = "UPDATE Animais SET primeira_ida = %s WHERE id = %s"
+            data = (
+                primeira_ida,
+                id
+            )
+        elif exclusividade == 'ultima_ida':
+            sql = "UPDATE Animais SET ultima_ida = %s WHERE id = %s"
+            data = (
+                ultima_ida,
+                id
+            )
+        elif exclusividade == 'castrado':
+            sql = "UPDATE Animais SET castrado = %s WHERE id = %s"
+            data = (
+                castrado,
                 id
             )
         elif exclusividade == 'especie_id':
@@ -147,15 +214,108 @@ def update_BD(id, nome, alimentacao=None, data_nasc=None, peso=None, cor=None, e
                 id
             )
         else:
-            sql = "UPDATE Animais SET nome = %s, data_nasc = %s, peso = %s, cor = %s, especie_id = %s WHERE id = %s"
+            sql = "UPDATE Animais SET nome = %s, data_nasc = %s, peso = %s, pelagem = %s, sexo = %s, primeira_ida= %s, ultima_ida = %s, castrado = %s, especie_id = %s WHERE id = %s"
             data = (
                 nome,
                 data_nasc,
                 peso,
-                cor,
+                pelagem,
+                sexo,
+                primeira_ida,
+                ultima_ida,
+                castrado,
                 especie_id,
                 id
             )
+
+    elif tabela == 'Cliente':
+        if exclusividade == 'nome':
+            sql = "UPDATE Cliente SET nome = %s WHERE cpf = %s"
+            data = (
+                nome,
+                cpf
+            )
+
+        elif exclusividade == 'logradouro':
+            sql = "UPDATE Cliente SET logradouro = %s WHERE cpf = %s"
+            data = (
+                logradouro,
+                cpf
+            )
+
+        elif exclusividade == 'numero':
+            sql = "UPDATE Cliente SET numero = %s WHERE cpf = %s"
+            data = (
+                numero,
+                cpf
+            )
+
+        elif exclusividade == 'bairro':
+            sql = "UPDATE Cliente SET bairro = %s WHERE cpf = %s"
+            data = (
+                bairro,
+                cpf
+            )
+
+        elif exclusividade == 'cidade':
+            sql = "UPDATE Cliente SET cidade = %s WHERE cpf = %s"
+            data = (
+                cidade,
+                cpf
+            )
+
+        elif exclusividade == 'estado':
+            sql = "UPDATE Cliente SET estado = %s WHERE cpf = %s"
+            data = (
+                estado,
+                cpf
+            )
+
+        elif exclusividade == 'animal_id':
+            sql = "UPDATE Cliente SET animal_id = %s WHERE cpf = %s"
+            data = (
+                animal_id,
+                cpf
+            )
+
+        else:
+            sql = "UPDATE Cliente SET nome = %s, logradouro = %s, numero = %s, bairro = %s, cidade = %s, estado = %s, animal_id = %s WHERE cpf = %s"
+            data = (
+                nome,
+                logradouro,
+                numero,
+                bairro,
+                cidade,
+                estado,
+                animal_id,
+                cpf
+            )
+        
+    elif tabela == 'Raca':
+        sql = "UPDATE Raca SET nome = %s WHERE id = %s"
+        data = (
+            nome,
+            id
+   
+    elif tabela == 'Telefone':
+        if exclusividade == 'nome':
+            sql = "UPDATE Telefone SET  = %s WHERE id = %s"
+            data = (
+                nome,
+                id
+
+    elif tabela == 'Email':
+        if exclusividade == 'nome':
+            sql = "UPDATE Email SET  = %s WHERE id = %s"
+            data = (
+                nome,
+                id
+           
+    
+    
+
+    else:
+        print("Algo deu errado, Tabela incorreta.")
 
     cursor.execute(sql, data) #Executa o comando SQL
     connection.commit()
@@ -169,7 +329,7 @@ def update_BD(id, nome, alimentacao=None, data_nasc=None, peso=None, cor=None, e
 
 #DELETE
 def delete_BD(tabela, id):
-    connection = conectarBD("localhost", "root", , "Login")
+    connection = conectarBD("localhost", "root", "admin", "Login")
     cursor = connection.cursor()
 
     sql = "DELETE FROM %s WHERE id = %s"
@@ -187,152 +347,3 @@ def delete_BD(tabela, id):
     connection.close() #Fecha a conexão com o banco
 
     print(recordsaffected, " registros excluídos")
-
-"""
-while True:
-    os.system("cls")
-    print("\n ::::: APLICACA0 D3 GERENCI4MENT0 D0 BD MySQL ::::: \n")
-    print(" Digite: ")
-    print("1 - Inserir na tabela (INSERT)")
-    print("2 - Leitura da tabela (SELECT)")
-    print("3 - Atualizacao da tabela (UPDATE)")
-    print("4 - Remocao da tabela (DELETE)")
-    opcao = int(input("iex> "))
-
-    if opcao == 1:
-        print("\n Cadastrar ...\n1 - Animal \n ou \n2 -Especie?")
-        opcao1 = int(input("iex> "))
-        if opcao1 == 1:
-            nome = input("Escreva o nome a ser cadastrado: ")
-            data_nasc = input("Escreva a data de nescimento a ser cadastrada: ")
-            peso = input("Escreva o peso a ser cadastrado: ")
-            cor = input("Escreva a cor a ser cadastrada: ")
-            especie_id = input("Escreva o id da especie a ser cadastrado: ")
-            insert_BD(nome, data_nasc=data_nasc, peso=peso, cor=cor, especie_id=especie_id)
-        elif opcao1 == 2:
-            nome = input("Escreva o nome a ser cadastrado: ")
-            alimentacao = input("Escreva a alimentacao a ser cadastrada: ")
-            insert_BD(nome, alimentacao)
-        else:
-            print("Erro na digitacao! Retornar ao inicio.")
-        time.sleep(3)
-
-    elif opcao == 2:
-        print("\n Qual tabela voce quer selecionar? ")
-        print("1 - Animal \n2 - Especie")
-        tabela = int(input("iex> "))
-        if tabela == 1:
-            read_BD("Animais")
-        elif tabela == 2:
-            read_BD("Especies")
-        else:
-            print("Erro na digitacao! Retornar ao inicio.")
-        time.sleep(3)
-
-    elif opcao == 3:
-        print("\n ::: Voce quer atualizar o que? :::")
-        print("1   - Apenas nome (Animais)")
-        print("11  - Apenas nome (Especies)")
-        print("2   - Apenas alimentacao")
-        print("3   - Apenas data de nescimento")
-        print("4   - Apenas peso")
-        print("5   - Apenas cor")
-        print("6   - Apenas id da especie")
-        print("12  - Nome e alimentacao")
-        print("136 - Nome, data de nescimento, peso, cor e id da especie")
-        opcao3 = int(input("iex> "))
-
-        if opcao3 == 1:
-            IDd = input("ID que voce quer atualizar: ")
-            nome = input("Escreva o nome a ser atualizado: ")
-            update_BD(IDd, nome, exclusividade='nome')
-            time.sleep(3)
-
-        elif opcao3 == 11:
-            IDd = input("ID que voce quer atualizar: ")
-            nome = input("Escreva o nome a ser atualizado: ")
-            update_BD(IDd, nome, alimentacao="", exclusividade='nome')
-            time.sleep(3)
-
-        elif opcao3 == 2:
-            IDd = input("ID que voce quer atualizar: ")
-            alimentacao = input("Escreva a alimentacao a ser atualizada: ")
-            nome = ""
-
-            update_BD(IDd, nome, alimentacao=alimentacao, exclusividade='alimentacao')
-            time.sleep(3)
-
-        elif opcao3 == 3:
-            IDd = input("ID que voce quer atualizar: ")
-            data_nasc = input("Escreva a data de nascimento a ser atualizada: ")
-            nome = ""
-
-            update_BD(IDd, nome, data_nasc=data_nasc, exclusividade='data_nasc')
-            time.sleep(3)
-
-        elif opcao3 == 4:
-            IDd = input("ID que voce quer atualizar: ")
-            peso = input("Escreva o peso a ser atualizado: ")
-            nome = ""
-
-            update_BD(IDd, nome, peso=peso, exclusividade='peso')
-            time.sleep(3)
-        
-        elif opcao3 == 5:
-            IDd = input("ID que voce quer atualizar: ")
-            cor = input("Escreva a cor a ser atualizada: ")
-            nome = ""
-
-            update_BD(IDd, nome, peso=peso, exclusividade='peso')
-            time.sleep(3)
-        
-        elif opcao3 == 6:
-            IDd = input("ID que voce quer atualizar: ")
-            id_especie = input("Escreva o id da especie a ser atualizado: ")
-            nome = ""
-
-            update_BD(IDd, nome, especie_id=id_especie, exclusividade='especie_id')
-            time.sleep(3)
-
-        elif opcao3 == 12:
-            IDd = input("ID que voce quer atualizar: ")
-            alimentacao = input("Escreva a alimentacao a ser atualizada: ")
-            nome = input("Escreva o nome a ser atualizado: ")
-
-            update_BD(IDd, nome, alimentacao=alimentacao)
-            time.sleep(3)
-
-        elif opcao3 == 136:
-            IDd = input("ID que voce quer atualizar: ")
-            nome = input("Escreva o nome a ser atualizado: ")
-            data_nasc = input("Escreva a data de nascimento a ser atualizada: ")
-            peso = input("Escreva o peso a ser atualizado: ")
-            cor = input("Escreva a cor a ser atualizada: ")
-            id_especie = input("Escreva o id da especie a ser atualizado: ")
-
-            update_BD(IDd, nome, data_nasc=data_nasc, peso=peso, cor=cor, especie_id=id_especie)
-            time.sleep(3)
-
-        else:
-            print("Numero digitado errado")
-            time.sleep(3)
-        
-    elif opcao == 4:
-        print("Escreva a tabela que contem o que voce quer apagar:")
-        tabela = int(input("iex> "))
-        print("Escreva o id que voce quer apagar:")
-        IDd = int(input("iex> "))
-        
-        delete_BD(tabela, IDd)
-        time.sleep(3)
-
-    else:
-        print("Numero digitado errado")
-        time.sleep(3)
-
-    opcao2 = input("Quer realizar mais alguma acao? [y/n] ").lower()
-    if opcao2 == 'y':
-        pass
-    else:
-        break
-"""
