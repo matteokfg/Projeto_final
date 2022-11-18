0cf27c77cd95d94fa02f506086a07e99c99e2fc7
Matteo K. F. Giuliani <67129306+matteokfg@users.noreply.github.com>
2022-11-18 10:05:53 -0300

    Update banco_de_dados.sql

--- projeto/mysql/banco_de_dados.sql
+++ projeto/mysql/banco_de_dados.sql
@@ -3,7 +3,7 @@ use PetShop;                      -- Seleciona o banco para os próximos comando
 /* As linhas acima não devem ser executas em serviços online como o sqlite oline*/
 create table Raca (
 	id		integer		primary key auto_increment,
-    nome	varchar(60)
+    nome	varchar(60)	unique not null
 );
 
 create table Especies (
@@ -12,38 +12,44 @@ create table Especies (
 	alimentacao		varchar(20),
     raca_id			integer				references Raca(id)
 );
+
 create table Animais (
 	id				integer 			primary key auto_increment,
 	nome			varchar(50) 		not null,
 	data_nasc		date 				not null,
 	peso			decimal(10,2)		check (peso > 0),
-	pelagem			varchar(50),
-    sexo			char(1),
-    primeira_ida	date,
-    ultima_ida		date,
-    castrado		boolean,
-	especie_id		int					references Especies(id)
+	pelagem			varchar(50)			not null,
+    sexo			char(1)				not null,
+    primeira_ida	date				check (primeira_ida < curdate()), /* ver se eh ativo */
+    ultima_ida		date				check (ultima_ida <= curdate()),
+    castrado		boolean				not null,
+	especie_id		integer				references Especies(id)
+); /* curdate() */
+
+create table Cliente (
+	cpf			char(11)		primary key,
+    nome		varchar(60) 	not null,
+    logradouro	varchar(100)	not null,
+    numero		varchar(8)		not null,
+    bairro		varchar(30),
+    cidade		varchar(50)		not null,
+    estado		char(2)			not null, /* colocar em letra maiuscula (.upper)) */
+	animal_id	integer			references Animais(id)
 );
+
 create table Telefone (
-	dono_id		int,
+	cliente_id	integer,
     ddd			char(2),
     telefone	char(9),
     
-    primary key(dono_id, ddd, telefone),
-    foreign key(dono_id) references Cliente(cpf)
+    primary key(cliente_id, ddd, telefone),
+    foreign key(cliente_id) references Cliente(cpf)
 );
+
 create table Email (
-	cliente_id	int,
+	cliente_id	integer,
     email		varchar(100),
     
     primary key(cliente_id, email),
     foreign key(cliente_id) references Cliente(cpf)
-);
-create table Cliente (
-	cpf			char(11)		primary key,
-    nome		varchar(60) 	not null,
-    logradouro	varchar(100),
-    numero		varchar(8),
-    bairro		varchar(30),
-	animal_id	int				references Animais(id)
 );
