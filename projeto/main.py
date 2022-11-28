b85d7946b63c199bd970357f297515c2540653de
matteokfg <67129306+matteokfg@users.noreply.github.com>
2022-11-28 17:04:16 -0300

    Create main.py

---
+++ projeto/main.py
@@ -0,0 +1,31 @@
+from PyQT5 import uic, QtWidgets
+import sys
+import os
+import mysql.connector
+import crud as Crud # importa as funcoes do documento crud.py
+# os.system("pip install mysql-connector-python")
+
+#-- INICIO ------------ FUNCOES DE VALIDACAO E DE BACKEND, ENTRE TELAS E CRUD -------------------
+
+#-- FIM ------------ FUNCOES DE VALIDACAO E DE BACKEND, ENTRE TELAS E CRUD ----------------------
+
+
+#-- INICIO ------------ CONFIGURACAO DAS TELAS PARA DEPOIS EXECUTAR, INCLUI VINCULACAO DAS FUNCOES COM OS BOTOES -----------------------------
+
+# app = QtWidgets.QApplication(sys.argv)
+# tela_s = uic.loadUi('nome_tela.ui')
+# tela_s = uic.loadUi('nome_tela.ui')
+# tela_s = uic.loadUi('nome_tela.ui')
+# tela_s = uic.loadUi('nome_tela.ui')
+# tela_s = uic.loadUi('nome_tela.ui')
+# tela_s = uic.loadUi('nome_tela.ui')
+# tela_s = uic.loadUi('nome_tela.ui')
+# tela_inicial = uic.loadUi('nome_tela_inicial.ui')
+
+# tela_inicial.show()
+
+#-- FIM ------------ CONFIGURACAO DAS TELAS PARA DEPOIS EXECUTAR, INCLUI VINCULACAO DAS FUNCOES COM OS BOTOES -------------------------------
+
+# app.exec()
+
+
